1174bc967db9aae1de9a3c78c4edae08339ca698
zhangyuhui1 <596943130@qq.com>
2019-01-29 10:43:37 +0800

    c3_3_b.c

--- c3_3_b.c
+++ c3_3_b.c
@@ -1,20 +1,32 @@
-#include<stdio.h>
-#include"DoublyList.h"
+#include <stdio.h>
+#include "DoublyList.h"
+
+List DoublyListSwap(List L);
 
 int main()
 {
 	List L;
 	int n,i;
-	printf("Please enter the number of L,n=");
-	scanf("%d",&n);
+	n = 2;
+	printf("Please enter the element of L\n");
 	L = CreateList(n);
-	for(i = 0; i < n; i++){
-		while(L->Next != NULL){
-			L = L->Next;
-			printf("%3d",L->Element);
-		}
+	PrintList(L);
+	L = DoublyListSwap(L);
+	PrintList(L);
 	return 0;
 }
 
 
-
+List DoublyListSwap(List L)
+{
+	Position P;
+	P = L->Next;
+	P = P->Next;
+	P->Next = L->Next;
+	L->Next = P;
+	P->Prev = L;
+	P = P->Next;
+	P->Prev = L->Next;
+	P->Next = NULL;
+	return L;
+}
