e38e40a7e7be0119081dd54802bf768eab40297b
zhangyuhui1 <596943130@qq.com>
2019-01-30 16:27:27 +0800

    c3_5.c

--- c3_5.c
+++ c3_5.c
@@ -19,34 +19,20 @@ int main()
 	PrintList(L2);
 	L = UnionList(L1,L2);
 	printf("The union of L1 and L2:");
-<<<<<<< HEAD
-	while(L->Next != NULL){
-		L = L->Next;
-		printf("%3d",L->Element);
-	} 	
-	printf("\n");
-=======
-	PrintList(L);	
-	
->>>>>>> 953823cd98a1ed82d1417c5bd381f11a8437142e
+	PrintList(L);
 }
 
 
 List UnionList(List L1, List L2)
 {
 	List L;
-<<<<<<< HEAD
 	Position P;
-=======
-	Position P,Tem1,Tem2,Tem;
->>>>>>> 953823cd98a1ed82d1417c5bd381f11a8437142e
 	P = malloc(sizeof(struct Node));
 	if(P == NULL)
 		FatalError("Out of space!!!");
 	L = malloc(sizeof(struct Node));
 	if(L == NULL)
 		FatalError("Out of space!!!");
-<<<<<<< HEAD
 	L1 = L1->Next;
 	L2 = L2->Next;
 	P = L;
@@ -78,64 +64,3 @@ List UnionList(List L1, List L2)
 	return L;
 }
 
-=======
-	Tem1 = malloc(sizeof(struct Node));
-	if(Tem1 == NULL)
-		FatalError("Out of space!!!");
-	Tem2 = malloc(sizeof(struct Node));
-	if(Tem2 == NULL)
-		FatalError("Out of space!!!");
-//	L1 = L1->Next;
-//	L2 = L2->Next;
-	L->Next = P;
-	while(L1->Next != NULL && L2->Next != NULL){
-		Tem1 = L1->Next;
-		Tem2 = L2->Next;
-		Tem = malloc(sizeof(struct Node));
-		if(Tem == NULL)
-			FatalError("Out of space!!!");
-		if(Tem1->Element < Tem2->Element){
-			L1 = L1->Next;
-			P->Element = L1->Element;
-		}
-		else if(Tem1->Element > Tem2->Element){
-			L2 = L2->Next;
-			Insert(L2->Element, L, P);
-//			P->Element = L2->Element;
-		}
-		else{
-			L1 = L1->Next;
-			L2 = L2->Next;
-			Insert(L1->Element, L, P);
-//			P->Element = L1->Element;
-		}
-		P =P->Next;
-//		P->Next = Tem;
-//		P = Tem;	
-	}
-	while(L1->Next == NULL){
-		Tem = malloc(sizeof(struct Node));
-		if(Tem == NULL)
-			FatalError("Out of space!!!");
-		L2 = L2->Next;
-		Insert(L2->Element, L ,P);
-//		P->Element = L2->Element;
-		P = P->Next;
-//		P->Next = Tem;
-//		P = Tem;
-	}
-	while(L2->Next == NULL){
-		Tem = malloc(sizeof(struct Node));
-		if(Tem == NULL)
-			FatalError("Out of space!!!");
-		L1 = L1->Next;
-//		Insert(L1->Element, L, P);
-
-		P->Element = L1->Element;
-		P =P->Next;
-//		P->Next = Tem;
-//		P = Tem;
-	}
-	return L;
-}
->>>>>>> 953823cd98a1ed82d1417c5bd381f11a8437142e
