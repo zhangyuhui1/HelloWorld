6bfa98fbf15b052cbe4a96248c370da270a7e593
zhangyuhui1 <596943120@qq.com>
2019-02-16 21:00:53 +0800

    Polynomial.h

--- Polynomial.h
+++ Polynomial.h
@@ -9,6 +9,7 @@ typedef PtrToNode Polynomial;
 Polynomial CreatePolynomial(int n);
 Polynomial AddPolynomial(Polynomial Poly1, Polynomial Poly2);
 void PrintPolynomial(Polynomial Poly);
+Polynomial MultPolynomial(Polynomial Poly1, Polynomial Poly2);
 
 #endif  /*_Polynomial_H*/
 
@@ -113,3 +114,52 @@ Polynomial AddPolynomial(Polynomial Poly1, Polynomial Poly2)
 		
 }
 
+
+/*Input two polynomial Poly1 and Poly2, return the result of 
+ *multiplication of two polynomials.
+ */
+Polynomial MultPolynomial(Polynomial Poly1, Polynomial Poly2)
+{
+	PtrToNode P,Tem;
+	Polynomial PolyMult,TemPoly,TemPoly1;
+	P = malloc(sizeof(struct Node));
+	if(P == NULL)
+		printf("Out of space!!!");
+	PolyMult = malloc(sizeof(struct Node));
+	if(PolyMult == NULL)
+		printf("Out of space!!!");
+	PolyMult->Next = NULL;
+	TemPoly = malloc(sizeof(struct Node));
+	if(TemPoly == NULL)
+		printf("Out of space!!!");
+	TemPoly1 = malloc(sizeof(struct Node));
+	if(TemPoly1 == NULL)
+		printf("Out of space!!!");
+	for(;Poly1->Next != NULL;){
+		Poly1 = Poly1->Next;
+		for(P = Poly2; P->Next != NULL;){
+			P = P->Next;
+			Tem = malloc(sizeof(struct Node));
+			if(Tem == NULL)
+				printf("Out of space!!!");
+			Tem->Coefficient = Poly1->Coefficient*P->Coefficient;
+			Tem->Exponent = Poly1->Exponent+P->Exponent;
+			for(TemPoly = PolyMult; TemPoly != NULL;){
+				TemPoly1 = TemPoly->Next;
+				if(TemPoly1 != NULL && Tem->Exponent == TemPoly1->Exponent){  //if the result is in the list,only add.
+					TemPoly1->Coefficient += Tem->Coefficient;
+					break;
+				}
+				else if(TemPoly1 != NULL && Tem->Exponent < TemPoly1->Exponent){ //if the exponent of the result less than the item, check the next item of the list.
+					TemPoly = TemPoly->Next;
+				}
+				else{  //if the exponent of the result more than the item, insert before the item.
+					Tem->Next = TemPoly->Next;
+					TemPoly->Next = Tem;
+					break;
+				}			
+			}
+		}
+	}	
+	return PolyMult;
+}	
